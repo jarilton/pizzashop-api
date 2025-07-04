d707874c0279267cd9696c99ef73c1857e9d2b0d
jariltonpereira <jarilton.pereira-terceiro@cea.com.br>
2025-07-04 11:31:47 -0300

    feat: create api pizza shop

---
+++ src/http/server.ts
@@ -0,0 +1,11 @@
+import { Elysia } from 'elysia';
+
+const app = new Elysia()
+.get('/', () => {
+  return 'Welcome to PizzaShop API!';
+})
+
+app.listen(3333, () => {
+  console.log('Server is running');
+});
+
