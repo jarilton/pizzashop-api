a93c372a7985334073c51416dc8c1b8b3b79f416
jariltonpereira <jarilton.pereira-terceiro@cea.com.br>
2025-07-24 16:15:04 -0300

    feat: rota de cadastro de restaurante

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,42 @@
 import { Elysia } from 'elysia'
+import { db } from '../db/connection'
+import { restaurants, users } from '../db/schema'
 
-const app = new Elysia().get('/', () => {
-  return 'Welcome to PizzaShop API!'
+const app = new Elysia().post('/restaurants', async ({ body, set }) => {
+  const { restaurantName, name, email, phone } = body as any
+
+  const [manager] = await db
+    .insert(users)
+    .values({
+      name,
+      email,
+      phone,
+      role: 'manager',
+    })
+    .returning({
+      id: users.id,
+    })
+
+  const [restaurant] = await db
+    .insert(restaurants)
+    .values({
+      name: restaurantName,
+      managerId: manager?.id,
+    })
+    .returning({
+      id: restaurants.id,
+      name: restaurants.name,
+      description: restaurants.description,
+      createdAt: restaurants.createdAt,
+      updatedAt: restaurants.updatedAt,
+    })
+
+  set.status = 204
+
+  return {
+    message: 'Restaurant created successfully',
+    restaurant,
+  }
 })
 
 app.listen(3333, () => {
