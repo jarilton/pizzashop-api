e22b88d6d90e83995322a678bf007334141b582c
jariltonpereira <jarilton.pereira-terceiro@cea.com.br>
2025-07-24 18:32:09 -0300

    feat: rota de envio do link de autenticação

---
+++ drizzle/0004_worried_abomination.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "auth_links" (
+	"id" text PRIMARY KEY NOT NULL,
+	"user_id" text NOT NULL,
+	"created_at" timestamp DEFAULT now()
+);
+--> statement-breakpoint
+ALTER TABLE "auth_links" ADD CONSTRAINT "auth_links_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
---
+++ src/db/schema/auth-links.ts
@@ -0,0 +1,13 @@
+import { createId } from '@paralleldrive/cuid2'
+import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+import { users } from './users'
+
+export const authLinks = pgTable('auth_links', {
+  id: text('id')
+    .$defaultFn(() => createId())
+    .primaryKey(),
+  userId: text('user_id')
+    .references(() => users.id)
+    .notNull(),
+  createdAt: timestamp('created_at').defaultNow(),
+})
