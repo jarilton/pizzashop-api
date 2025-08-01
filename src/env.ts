aacfadf584de53907bc39b9b8edd53c6873d24a0
jariltonpereira <jarilton.pereira-terceiro@cea.com.br>
2025-08-01 16:48:46 -0300

    feat: setup de autenticação JWT

--- src/env.ts
+++ src/env.ts
@@ -2,8 +2,9 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   API_BASE_URL: z.url().min(1),
-  AUTH_REDIRECT_URL: z.string().url().min(1),
+  AUTH_REDIRECT_URL: z.url().min(1),
   DATABASE_URL: z.url().min(1),
+  JWT_SECRET_KEY: z.string().min(1),
 })
 
 export const env = envSchema.parse(process.env)
