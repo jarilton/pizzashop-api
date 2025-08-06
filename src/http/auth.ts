c684984114ca5d344e8e946fea5f40701edb26c9
jariltonpereira <jarilton.pereira-terceiro@cea.com.br>
2025-08-06 17:12:48 -0300

    feat: rota de logout e derivação

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -1,19 +1,36 @@
 import cookie from '@elysiajs/cookie'
 import jwt from '@elysiajs/jwt'
-import Elysia, { t } from 'elysia'
+import Elysia, { t, type Static } from 'elysia'
 import { env } from '../env'
 
+const jwtPayload = t.Object({
+  sub: t.String(),
+  restaurantId: t.Optional(t.String()),
+})
+
 export const auth = () =>
-  new Elysia({
-    name: 'auth',
-  })
+  new Elysia({ name: 'auth' })
     .use(
       jwt({
         secret: env.JWT_SECRET_KEY,
-        schema: t.Object({
-          sub: t.String(),
-          restaurantId: t.Optional(t.String()),
-        }),
+        schema: jwtPayload,
       }),
     )
     .use(cookie())
+    .derive(({ jwt, setCookie, removeCookie }) => {
+      return {
+        signUser: async (payload: Static<typeof jwtPayload>) => {
+          const token = await jwt.sign(payload)
+
+          setCookie('auth', token, {
+            httpOnly: true,
+            maxAge: 60 * 60 * 24 * 7, // 7 days
+            path: '/',
+          })
+        },
+
+        signOut: () => {
+          removeCookie('auth')
+        },
+      }
+    })
--- src/http/routes/authenticate-from-link.ts
+++ src/http/routes/authenticate-from-link.ts
@@ -1,12 +1,14 @@
 import { Elysia, t } from 'elysia'
 import { db } from '../../db/connection'
 import dayjs from 'dayjs'
-import { auth } from '../auth'
+
 import { authLinks } from '../../db/schema'
 import { eq } from 'drizzle-orm'
-export const authenticateFromLink = new Elysia().use(auth()).get(
+import { auth } from '../auth'
+
+export const authenticateFromLink = new Elysia().use(auth).get(
   '/auth-links/authenticate',
-  async ({ query, jwt, setCookie, redirect }) => {
+  async ({ query, signUser, redirect }) => {
     const { code, redirect: redirectTo } = query
 
     const authLinkFromCode = await db.query.authLinks.findFirst({
@@ -34,15 +36,9 @@ export const authenticateFromLink = new Elysia().use(auth()).get(
       },
     })
 
-    const token = await jwt.sign({
+    await signUser({
       sub: authLinkFromCode.userId,
       restaurantId: managedRestaurant?.id,
-    })
-
-    setCookie('auth', token, {
-      httpOnly: true,
-      maxAge: 60 * 60 * 24 * 7, // 7 days
-      path: '/',
     })
 
     await db.delete(authLinks).where(eq(authLinks.code, code))
