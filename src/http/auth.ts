fc9edf69be915f48d33eeb38daf1c74ebebe09b0
jariltonpereira <jarilton.pereira-terceiro@cea.com.br>
2025-08-06 00:02:46 -0300

    feat: rota de autenticação pelo link

---
+++ src/http/auth.ts
@@ -0,0 +1,19 @@
+import cookie from '@elysiajs/cookie'
+import jwt from '@elysiajs/jwt'
+import Elysia, { t } from 'elysia'
+import { env } from '../env'
+
+export const auth = () =>
+  new Elysia({
+    name: 'auth',
+  })
+    .use(
+      jwt({
+        secret: env.JWT_SECRET_KEY,
+        schema: t.Object({
+          sub: t.String(),
+          restaurantId: t.Optional(t.String()),
+        }),
+      }),
+    )
+    .use(cookie())
